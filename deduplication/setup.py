0fe792bea5eeaf60c14cd95c34a68df158569b8b
bewakes <bewakepandey@gmail.com>
2020-08-11 12:46:49 +0545

    Make the setup.py better: change requirements

--- deduplication/setup.py
+++ deduplication/setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='deep_utils.deduplication',
-    version='1.0.0',
+    version='1.0.4',
     description='Document deduplication package for DEEP',
-    author='Bibek Pandey',
-    author_email='bibek.pandey@togglecorp.com',
-    packages=find_namespace_packages(),
-    license="MIT",
     long_description_content_type='text/markdown',
     long_description=open('./README.md').read(),
+    license="MIT",
+
+    author='Bibek Pandey',
+    author_email='bibek.pandey@togglecorp.com',
     url="https://github.com/the-deep/deep-server-utils",
+
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
@@ -18,6 +19,12 @@ setup(
         "Topic :: Utilities",
         "Operating System :: OS Independent",
     ],
-    install_requires=open('deep_utils/deduplication/requirements.txt').read().split(),
+
+    packages=find_namespace_packages(),
+    install_requires=[
+        'boto3==1.9.88',
+        'elasticsearch==7.8.1',
+        'requests-aws4auth==1.0',
+    ],
     zip_safe=False
 )
